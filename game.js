eb0f1d4b3f4b51d5197cacdd3242e3f51857aee8
fuquan99666 <1924293797@qq.com>
2025-06-26 10:49:37 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,42 @@
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
+
+const main = (currentTime) => {
+    if (gameOver) {
+        const score = snakeBody.length;
+        if (confirm(`Game over. Your score was ${score}. Play again?`)) {
+            window.location = window.location;
+        }
+        return;
+    }
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) {
+        return;
+    }
+
+    lastRenderTime = currentTime;
+    update();
+    SNAKE_SPEED=currentTime/3000+5;
+    draw();
+}
+
+window.requestAnimationFrame(main);
+
+const update = () => {
+    updateSnake();
+    updateFood();
+    checkGameOver();
+}
+
+const draw = () => {
+    gameBoard.innerHTML = "";
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+const checkGameOver = () => {
+    gameOver = snakeOutOfBounds() || snakeIntersectSelf();
+}
